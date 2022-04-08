df0066728b61cdd2725784e50cd90fabc8c64c49
danielvtfs <danielvtfs@gmail.com>
2022-04-08 07:17:20 -0300

    Primeiro commit

---
+++ src/components/SingInButton/index.tsx
@@ -0,0 +1,10 @@
+import { FaGithub } from "react-icons/fa";
+
+export function SingInButton() {
+  return (
+    <button>
+      <FaGithub />
+      Sing in with Gtihub
+    </button>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>react news</title>
+      </Head>
+      <h1>Hello Next</h1>
+    </>
+  );
+}
