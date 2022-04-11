2d4fce1a42e3738598bb4cf3b74d9a615beb2c56
danielvtfs <danielvtfs@gmail.com>
2022-04-11 06:27:00 -0300

    create header and signin button

--- src/components/SingInButton/index.tsx
+++ src/components/SingInButton/index.tsx
@@ -1,9 +1,20 @@
 import { FaGithub } from "react-icons/fa";
+import { FiX } from "react-icons/fi";
+
+import styles from "./styles.module.scss";
 
 export function SingInButton() {
-  return (
-    <button>
-      <FaGithub />
+  const isUserLoggedIn = true;
+
+  return isUserLoggedIn ? (
+    <button className={styles.singInButton}>
+      <FaGithub color="#04d361" />
+      Daniel Fernandez
+      <FiX color="#737380" className={styles.closeIcon} />
+    </button>
+  ) : (
+    <button className={styles.singInButton}>
+      <FaGithub color="#eba417" />
       Sing in with Gtihub
     </button>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,22 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>react news</title>
+        <title>hOME | react news</title>
       </Head>
-      <h1>Hello Next</h1>
+
+      <main>
+        <section>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for R$9,90</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="coding" />
+      </main>
     </>
   );
 }
